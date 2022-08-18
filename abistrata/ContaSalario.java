1f489f820c04ef7570dc294f4b89313dacaa8502
IgorEverton <103080377+IgorEverton@users.noreply.github.com>
2022-08-18 20:19:37 -0300

    Update ContaSalario.java
    Finalizando classe ContaSalario

--- abistrata/ContaSalario.java
+++ abistrata/ContaSalario.java
@@ -1,5 +1,32 @@
 package abistrata;
 
-public class ContaSalario {
+public class ContaSalario extends Conta{
+	private double salario;
+	private double saldo;
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
 
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	public ContaSalario() {
+		
+	}
+	public ContaSalario(double salario) {
+		this.salario=salario;
+	}
+	
+	public void Sacar(double valor) {
+		this.saldo-=valor;
+	}
 }
